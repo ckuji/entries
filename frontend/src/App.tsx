9a419e16ea4ddcecbd7702a127031a3de5433609
ckuji <may@email.com>
2023-08-16 17:34:56 +0500

    frontend webpack + react + typescript configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>My App</h1>
+        </div>
+    );
+}
+
+export default App;
